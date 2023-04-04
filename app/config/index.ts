390bf95e4a63f21f8e951347ced7328f9648554a
Javed Khan <live.javedkhan@gmail.com>
2023-04-05 00:18:02 +0530

    setup files updated

---
+++ app/config/index.ts
@@ -0,0 +1,4 @@
+export {default as colors} from './colors';
+export {default as screens} from './screens';
+export {default as fonts} from './fonts';
+export {default as shadow} from './shadow';
---
+++ app/config/shadow.ts
@@ -0,0 +1,13 @@
+import colors from './colors';
+
+export default {
+  shadowColor: colors.primary,
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.25,
+  shadowRadius: 3.84,
+
+  elevation: 5,
+};
